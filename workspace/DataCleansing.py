c08b4fadf3adeacc86a62ec57b65f857c98daa6c
oneboyi <88018678+oneboyi@users.noreply.github.com>
2023-05-07 19:56:06 +0800

    rename folder

---
+++ workspace/DataCleansing.py
@@ -0,0 +1,11 @@
+'''
+Author: oneboyi
+Date: 2023-05-04 22:09:33
+LastEditors: oneboyi
+LastEditTime: 2023-05-04 22:17:53
+FilePath: \sourcecodes\DataCleansing.py
+Description: 
+Copyright 2023 OBKoro1, All Rights Reserved. 
+2023-05-04 22:09:33
+'''
+import numpy
